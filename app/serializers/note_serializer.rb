5efc0a1e12dd4f922e7657e32bda00da4ea906fd
Rob <epr1935@icloud.com>
2017-03-26 16:53:47 -0400

    added :id to note serializer

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,6 +1,6 @@
 class NoteSerializer < ActiveModel::Serializer
 
-  attributes  :title, :body,
+  attributes  :title, :body, :id,
               :created_at, :updated_at
   has_many    :tags
   belongs_to  :user
